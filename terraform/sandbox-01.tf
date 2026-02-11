35fe660b1ebc84e54757dc03a41b5e7756287935
jabaral <117394484+jabaral@users.noreply.github.com>
2026-02-11 17:56:56 -0500

    dummy commit 02

--- terraform/sandbox-01.tf
+++ terraform/sandbox-01.tf
@@ -20,6 +20,7 @@ module "sandbox_account_01" {
     "ABC:Environment" = "Sandbox"
     "ABC:CostCenter"  = "123456"
     "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
     "ABC:BUCode"      = "ABC003"
     "ABC:Project"     = "123456"
   }

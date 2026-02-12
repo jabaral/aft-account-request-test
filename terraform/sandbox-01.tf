9bd7d8921fdd626aa657e5d38bcefdb4a1482da7
jabaral <117394484+jabaral@users.noreply.github.com>
2026-02-11 22:26:56 -0500

    remove

--- terraform/sandbox-01.tf
+++ terraform/sandbox-01.tf
@@ -1,38 +1,38 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-module "sandbox_account_01" {
-  source = "./modules/aft-account-request"
+# # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# # SPDX-License-Identifier: Apache-2.0
+# #
+# module "sandbox_account_01" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail = "jabaral+ct_sandbox_02_testing@amazon.com"
-    AccountName  = "sandbox-account-02"
-    # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail     = "jabaral+ct_sandbox_02_testing@amazon.com"
-    SSOUserFirstName = "Johan"
-    SSOUserLastName  = "Baron"
-  }
+#   control_tower_parameters = {
+#     AccountEmail = "jabaral+ct_sandbox_02_testing@amazon.com"
+#     AccountName  = "sandbox-account-02"
+#     # Syntax for top-level OU
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail     = "jabaral+ct_sandbox_02_testing@amazon.com"
+#     SSOUserFirstName = "Johan"
+#     SSOUserLastName  = "Baron"
+#   }
 
-  account_tags = {
-    "ABC:Owner"       = "jabaral+ct_sandbox_02_testing@amazon.com"
-    "ABC:Division"    = "ENT"
-    "ABC:Environment" = "Sandbox"
-    "ABC:CostCenter"  = "123456"
-    "ABC:Vended"      = "true"
-    "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
-  }
+#   account_tags = {
+#     "ABC:Owner"       = "jabaral+ct_sandbox_02_testing@amazon.com"
+#     "ABC:Division"    = "ENT"
+#     "ABC:Environment" = "Sandbox"
+#     "ABC:CostCenter"  = "123456"
+#     "ABC:Vended"      = "true"
+#     "ABC:BUCode"      = "ABC003"
+#     "ABC:Project"     = "123456"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "Johan Baron"
-    change_reason       = "testing the account vending process"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "Johan Baron"
+#     change_reason       = "testing the account vending process"
+#   }
 
-  custom_fields = {
-    custom1 = "a"
-    custom2 = "b"
-  }
+#   custom_fields = {
+#     custom1 = "a"
+#     custom2 = "b"
+#   }
 
-#   account_customizations_name = "sandbox-customizations"
-}
+# #   account_customizations_name = "sandbox-customizations"
+# }
